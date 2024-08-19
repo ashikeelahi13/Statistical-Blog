17cb43dabb83b53ed266d1411c8a0a013239f643
ashikeelai13 <learnwithashik3@gmail.com>
2024-08-20 02:49:52 +0600

    Add outline

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue">
+          <Button outline gradientDuoTone="purpleToBlue">
             Sign In
           </Button>
         </Link>
